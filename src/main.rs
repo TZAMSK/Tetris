aa4ae7255e7a1d6b4aaa4d02ba717c9c74b4030f
Alikhan Khakimov <akhakimov@dept-info.crosemont.quebec>
2024-11-18 01:02:31 -0500

    Main playground UI using sld2 made

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,8 @@ mod interface;
 mod piece;
 
 use engine::Engine;
-use interface::Interface;
 
 fn main() {
     let engine = Engine::new();
-
-    Interface::run(engine)
+    interface::run(engine)
 }
