de8feb4903d43304f3c5a7516424745176563198
Alikhan Khakimov <alikhankhakimov@outlook.com>
2024-11-17 20:30:00 -0500

    Project idea of the structure implemented

---
+++ src/engine/mod.rs
@@ -0,0 +1,21 @@
+pub struct Engine {
+    board: Board,
+}
+impl Engine {
+    pub fn new() -> Self {
+        Engine {
+            board: Board::blank(),
+        }
+    }
+}
+
+struct Board([bool; Self::WIDTH * Self::HEIGHT]);
+
+impl Board {
+    const WIDTH: usize = 10;
+    const HEIGHT: usize = 20;
+    const SIZE: usize = Self::WIDTH * Self::HEIGHT;
+    fn blank() -> Self {
+        Self([false; Self::SIZE])
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+#![allow(dead_code)]
+
+mod engine;
+mod interface;
+
+use engine::Engine;
+use interface::Interface;
+
 fn main() {
-    println!("Hello, world!");
+    let engine = Engine::new();
+    Interface::run(engine)
 }
